35b8ff4da7a47d5552c710be26e546e817792cf8
SHermukhammadjon <shermukhammadtemirov@gmail.com>
2023-10-21 14:50:25 +0500

    I fixsed chanel subscribtion check bug

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from aiogram import executor
 from loader import dp
 import handlers
 import middlewares
+import logging
 
 if __name__ == "__main__":
+
+    # logging.basicConfig()
     executor.start_polling(dp, skip_updates = False)
     
--- utilits/checker.py
+++ utilits/checker.py
@@ -2,11 +2,14 @@ from aiogram import Bot
 
 async def check_sub(user_id : int, chanel : str):
     try:
-        # bot = Bot.get_current()
-        # member  = await bot.get_chat_member(chat_id = chanel, user_id = user_id)
+        bot = Bot.get_current()
+        member  = await bot.get_chat_member(chat_id = chanel, user_id = user_id)
+        # print('-'*10)
+        # print(chanel)
         # print(member)
-        # if member.status != 'left':
-        #     return True
-        return True
+        # print(member.is_chat_member())
+        if member.status != 'left':
+            return True
+        return False
     except:
         return False
