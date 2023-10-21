797e0b69406d453536cfa321f7ed67f52f014bd4
SHermukhammadjon <shermukhammadtemirov@gmail.com>
2023-10-21 16:10:54 +0500

    I modified Bro function calback query responsb plart

--- middlewares/midle_checksub.py
+++ middlewares/midle_checksub.py
@@ -4,15 +4,13 @@ from aiogram.dispatcher.handler import CancelHandler
 from aiogram.dispatcher.middlewares import BaseMiddleware
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from loader import setting, bot, check_sub, ibuttons
+from loader import setting, bot, check_sub, ibuttons, dbuttons, ram
 
 
 class Bro(BaseMiddleware):
     async def on_pre_process_update(self, update : types.Update,  data: dict):        
         if update.message:
             id = update.message.from_user.id
-            
-            logging.info(id)
             buttons = {}
             status = True
             n = 0
@@ -33,26 +31,34 @@ class Bro(BaseMiddleware):
 
         elif update.callback_query:
             id = update.callback_query.from_user.id
-            logging.info(id)
             buttons = {}
             status = True
             n = 0
 
             for chanel, url in setting.data['forced_chanels'].items():
                 n += 1
-
                 if await check_sub(user_id = id, chanel = chanel):
                     buttons[f"✅ {n}-kanal"] = url 
                 else:
                     status = False
                     buttons[f"{n}-kanal"] = url 
-        
 
-            if not status:
-                await update.callback_query.answer(text = "Ilimos hamma kanalarga obuna bo'ling")
+            # print(status)
+            if update.callback_query.data == 'check':
+                if status:
+                    return
+            
+                else:
+                    await update.callback_query.answer("Iltimos hamma kanallarga obuna bo'ling", show_alert = True)
+                    raise CancelHandler()
+            
+            elif status:
+                return
+            
+            else:
+                await bot.delete_message(chat_id = id, message_id = update.callback_query.message.message_id)
+                await bot.send_message(text = "Iltimos quydagi kanallarga obuna bo'ling", disable_web_page_preview = True, chat_id = id, reply_markup = ibuttons.subscribe_chanels(buttons))
                 raise CancelHandler()
-        # else:
-        #     pass
         
 
         
